b910207e1ba7d4bdb2895500fd49577404719cd2
mateuzrib <mateuzrib@gmail.com>
2024-07-09 21:14:24 -0300

    add activities modal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,143 +1,50 @@
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
+import { ImportantLinks } from "./important-links";
+import { Guests } from "./guests";
+import { Activities } from "./activities";
+import { DestinationAndDateHeader } from "./destination-and-date-header";
 
 export function TripDetailsPage() {
+
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen ] = useState(false)
+
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true)
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
+
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-      {/* HEADER START */}
-      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <MapPin className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">Florianópolis, Brazil</span>
-        </div>
-
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <span className="text-zinc-100">August 17 to 23</span>
-          </div>
-
-          <div className="w-px h-6 bg-zinc-800"/>
-
-          <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-            Change locale/date
-            <Settings2 className="size-5" />
-          </button>
-        </div>
-
-      </div>
-      {/* HEADER END */}
-
-
+      <DestinationAndDateHeader />
       <main className="flex gap-16 px-6">
-        {/* ACTIVITIES START */}
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Activities</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={openCreateActivityModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <Plus className="size-5" />
               Create activity
             </button>
           </div>
 
-          <div className="space-y-8">
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">Day 17</span>
-                <span className="text-xs text-zinc-500">Saturday</span>
-              </div>
-              <p className="text-zinc-500 text-sm">No activity registered on this date.</p>
-            </div>
-
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">Day 18</span>
-                <span className="text-xs text-zinc-500">Sunday</span>
-              </div>
-
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300"/>
-                  <span className="text-zinc-100">Go-kart racing</span>
-                  <span className="text-zinc-400 text-sm ml-auto">02:00 pm</span>
-                </div>
-              </div>
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300"/>
-                  <span className="text-zinc-100">Go-kart racing</span>
-                  <span className="text-zinc-400 text-sm ml-auto">02:00 pm</span>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
+          <Activities />
         </div>
-        {/* ACTIVITIES START */}
-
         <div className="w-80 space-y-6">
-          <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Important links</h2>
-
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">AirBnB Reservation</span>
-                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011</a>
-                </div>
-                <Link2 className="text-zinc-400 size-5 shrink-0" />
-              </div>
-
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">House rules</span>
-                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                    https://www.notion.com/pages/1047000112354648336?adults=13&children=0&infants=0&pets=0&wishlist_item_id=11003621872995&check_in=2024-08-17&check_out=2024-08-26&source_impression_id=p3_1717600906_P3DL0E-bJZzguEci&previous_page_section_name=1000
-                  </a>
-                </div>
-                <Link2 className="text-zinc-400 size-5 shrink-0" />
-              </div>
-            </div>
-            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
-              <Plus className="size-5" />
-              Add new link
-            </button>
-          </div>
-
+          <ImportantLinks />
           <div className="w-full h-px bg-zinc-800"/>
-
-          <div className="space-y-6">
-          <h2 className="font-semibold text-xl">Guests</h2>
-
-          <div className="space-y-5">
-            <div className="flex items-center justify-between gap-4">
-              <div className="space-y-1.5">
-                <span className="block font-medium text-zinc-100">Jessica White</span>
-                <span className="block text-sm text-zinc-400 truncate">
-                  jessica.white44@yahoo.com
-                </span>
-              </div>
-              <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-            </div>
-
-            <div className="flex items-center justify-between gap-4">
-              <div className="space-y-1.5">
-                <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
-                <span className="block text-sm text-zinc-400 truncate">
-                  marian.hyatt@hotmail.com
-                </span>
-               </div>
-               <CircleCheck className="text-lime-300 size-5 shrink-0" />
-              </div>
-            </div>
-            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
-              <UserCog className="size-5" />
-              Manage guests
-            </button>
-          </div>
-          
+          <Guests />
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} />
+      )}
     </div>
   )
 }
