56e818234bfbba64ab4d351b0d2784832e751514
mateuzrib <mateuzrib@gmail.com>
2024-07-09 22:04:55 -0300

    add button components and replace occurrences

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean
@@ -27,15 +28,15 @@ export function DestinationAndDateStep({
       <div className="w-px h-6 bg-zinc-800"/>
 
       { isGuestsInputOpen ? (
-        <button onClick={closeGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+        <Button onClick={closeGuestsInput} variant='secondary'>
           Change locale/date
           <Settings2 className="size-5" />
-        </button>
-      ) : (
-        <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+        </Button>
+      ) : (      
+        <Button onClick={openGuestsInput} variant='primary'>
           Continue
           <ArrowRight className="size-5" />
-        </button>
+        </Button>
       )}
     </div>
   )
